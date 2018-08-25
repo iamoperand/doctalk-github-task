6ae9cefc8ee1baba4b1d5f58aa8e189ad08a50e2
Nikhil Arora <narora200@gmail.com>
2018-08-25 12:01:18 +0530

    [Feat]: Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        DocTalk - GitHub task
+      </div>
+    );
+  }
+}
+
+export default App;
